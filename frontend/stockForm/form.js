57823e81c110f949bd5dca560fbb7194ecb78034
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2025-06-21 13:49:01 +0200

    added form functionality to send data to a DB

--- frontend/stockForm/form.js
+++ frontend/stockForm/form.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("stockForm");
+
   if (form) {
     form.addEventListener("submit", function (e) {
       e.preventDefault();
+
       const formData = new FormData(form);
       const data = {};
       formData.forEach((value, key) => {
         data[key] = value;
       });
-      console.log(data);
-      alert("Data has been captured!");
+
+      fetch("backend-route", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("Success", result);
+        })
+        .catch((error) => {
+          console.error("Error", error);
+        });
     });
   }
 });
